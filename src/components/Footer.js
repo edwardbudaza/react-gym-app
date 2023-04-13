3e94cd8346aeba246e82aafe6307daaeb481d43f
edwardbudaza <edward.budaza@yahoo.co.za>
2023-04-13 08:25:13 +0200

    Changed the logo size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <Box mt="80px" bgcolor="#fff3f4">
       <Stack gap="40px" alignItems="center" px="40px" pt="24px">
-        <img src={Logo} alt="logo" width="150px" height="150px" />
+        <img src={Logo} alt="logo" width="150px" height="41px" />
         <Typography variant="h5" pb="40px" mt="20px">
           Made with ❤️ For Gym Lovers WorldWide
         </Typography>
